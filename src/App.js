d55dab3228659dc5a417db9772a3411df0dfb892
Vladislav Ustinkin <vlad.fast146@gmail.com>
2022-09-30 11:09:35 +0200

    solved localStorage challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(
+    JSON.parse(localStorage.getItem("counter")) || 0
+  );
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+  function decrement() {
+    if (counter === 0) return;
+    setCounter(counter - 1);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("counter", JSON.stringify(counter));
+  }, [counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter with local storage</h1>
+      <span>{counter}</span>
+      <button onClick={increment}>+ 1</button>
+      <button onClick={decrement}>- 1</button>
     </div>
   );
 }
